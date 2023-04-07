763f4e4759c218b49ca8936e01c8bc1af0dd2434
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2023-04-07 13:04:28 +0200

    Added email value to the Home component for later use

--- Greener/DAL/PlantDAL.cs
+++ Greener/DAL/PlantDAL.cs
@@ -6,89 +6,90 @@ using System.Data;
 
 namespace Greener.DAL
 {
-    public class PlantDAL : IPlantDAT
-    {
-        string projectId;
-        FirestoreDb firestoreDb;
+	public class PlantDAL : IPlantDAT
+	{
+		string projectId;
+		FirestoreDb firestoreDb;
 
-        public PlantDAL ( )
-        {
-            string dbFilePath = "C:\\Users\\migli\\source\\greener---the-palnt-manager-firebase-adminsdk-7vhar-5ca5a2aaee.json";
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", dbFilePath);
-            projectId = "greener---the-palnt-manager";
-            firestoreDb = FirestoreDb.Create(projectId);
+		public PlantDAL()
+		{
+			string dbFilePath = "C:\\Users\\migli\\source\\greener---the-palnt-manager-firebase-adminsdk-7vhar-5ca5a2aaee.json";
+			Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", dbFilePath);
+			projectId = "greener---the-palnt-manager";
+			firestoreDb = FirestoreDb.Create(projectId);
 
-        }
-        public async void AddPlant (Plant plant)
-        {
-            try
-            {
+		}
+		public async void AddPlant(Plant plant)
+		{
+			try
+			{
 
-                CollectionReference collectionReference = firestoreDb.Collection("Plants");
-                await collectionReference.AddAsync(plant);
-            }
-            catch ( Exception )
-            {
+				CollectionReference collectionReference = firestoreDb.Collection("Plants");
+				await collectionReference.AddAsync(plant);
+			}
+			catch (Exception)
+			{
 
-                throw;
-            }
-        }
+				throw;
+			}
+		}
 
-        public async Task<List<Plant>> AllPlants ( )
-        {
-            try
-            {
-                Query allPlantsQ = firestoreDb.Collection("Plants");
-                QuerySnapshot allPlantsSnap = await allPlantsQ.GetSnapshotAsync();
-                List<Plant> plants = new List<Plant>();
-                foreach ( DocumentSnapshot document in allPlantsSnap.Documents )
-                {
-                    if ( document.Exists )
-                    {
-                        Dictionary<string, object> dictionary = document.ToDictionary();
-                        var json = JsonConvert.SerializeObject(dictionary);
-                        Plant newPlant = JsonConvert.DeserializeObject<Plant>(json);
-                        newPlant.Id = document.Id;
-                        plants.Add(newPlant);
+		public async Task<List<Plant>> AllPlants()
+		{
+			try
+			{
 
-                    }
-                }
-                List<Plant> sortedPlants = plants.OrderBy(n => n.Name).ToList();
-                return sortedPlants;
-            }
-            catch ( Exception )
-            {
+				Query allPlantsQ = firestoreDb.Collection("Plants");
+				QuerySnapshot allPlantsSnap = await allPlantsQ.GetSnapshotAsync();
+				List<Plant> plants = new List<Plant>();
+				foreach (DocumentSnapshot document in allPlantsSnap.Documents)
+				{
+					if (document.Exists)
+					{
+						Dictionary<string, object> dictionary = document.ToDictionary();
+						var json = JsonConvert.SerializeObject(dictionary);
+						Plant newPlant = JsonConvert.DeserializeObject<Plant>(json);
+						newPlant.Id = document.Id;
+						plants.Add(newPlant);
 
-                throw;
-            }
-        }
+					}
+				}
+				List<Plant> sortedPlants = plants.OrderBy(n => n.Name).ToList();
+				return sortedPlants;
+			}
+			catch (Exception)
+			{
 
-        public async void RemovePlant (string Id)
-        {
-            try
-            {
-                DocumentReference documentReference = firestoreDb.Collection("Plants").Document(Id);
-                await documentReference.DeleteAsync();
-            }
-            catch ( Exception )
-            {
+				throw;
+			}
+		}
 
-                throw;
-            }
-        }
+		public async void RemovePlant(string Id)
+		{
+			try
+			{
+				DocumentReference documentReference = firestoreDb.Collection("Plants").Document(Id);
+				await documentReference.DeleteAsync();
+			}
+			catch (Exception)
+			{
 
-        public async void UpdatePlant (Plant plant)
-        {
-            try
-            {
-                DocumentReference documentReference = firestoreDb.Collection("Plants").Document(plant.Id);
-                await documentReference.SetAsync(plant, SetOptions.Overwrite);
-            }
-            catch ( Exception )
-            {
+				throw;
+			}
+		}
 
-                throw;
-            }
-        }
-    }
+		public async void UpdatePlant(Plant plant)
+		{
+			try
+			{
+				DocumentReference documentReference = firestoreDb.Collection("Plants").Document(plant.Id);
+				await documentReference.SetAsync(plant, SetOptions.Overwrite);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+	}
 }
